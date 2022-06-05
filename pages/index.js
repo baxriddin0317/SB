cf21b165dc6abacb41bdab83160dfecafb65f09d
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2022-06-05 12:11:09 +0500

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
-import About from "../Components/About";
-import Content from "../Components/Content";
-import Main from "../Components/Main";
-import Projects from "../Components/Projects";
-import Skills from "../Components/Skills";
+import About from "../components/About";
+import Content from "../components/Content";
+import Main from "../components/Main";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
